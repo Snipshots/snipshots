2501831103aa642c0dacdf497c74fc85dffb8379
Kris Fragata <kristinnefragata@outlook.com>
2024-01-19 20:07:15 -0500

    setting up boilerplate code 1.19

---
+++ back-end/server.js
@@ -0,0 +1,1 @@
+console.log('connected to DB');
---
+++ front-end/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports ={
+  mode: process.env.NODE_ENV,
+  entry:{
+    bundle: path.resolve(__dirname, "src", "index.js")
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: '[name].js', // the brackets allow it to be named anything, points to entry.bundle
+    clean: true, // only compile bundle.js once
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html", //creates mirror files, this one is 1 line
+      template: "src/template.html",
+    }),
+  ],
+  devServer:{
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // // Creates `style` nodes from JS strings
+          "style-loader",
+          // // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        //allows images to be loaded
+        test: /\.(png|svg|jpg|jpeg|gif)/,
+        exclude: /node_modules/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
+    
