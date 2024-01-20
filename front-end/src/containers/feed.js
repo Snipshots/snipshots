1756fc5ea60850df221ed659c0bdb16e31f85897
Julien Kerekes <julien.kerekes@gmail.com>
2024-01-20 21:26:19 +0100

    Created a core layout for the frontend

---
+++ front-end/src/containers/feed.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; 
+
+//const Feed = ({dispatch}) => {}
+
+class Feed extends Component {
+    render () {
+        return (
+          <div id='overview'>
+            <h1> Hello! </h1>
+            <button> Create New Snip </button>
+          </div>
+        );
+    }
+}
+
+export default Feed; 
