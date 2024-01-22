93dd6bdc9fadec2b3a302c5bcae07b2df750450c
julien-kerekes <julien.kerekes@stud.hs-bochum.de>
2024-01-22 08:24:27 +0100

    Added snippetReducer to rootReducer

--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import feedReducer from './feedSlice';
+import snippetReducer from './snippetSlice';
 
 const rootReducer = combineReducers({
   feed: feedReducer,
+  snippet: snippetReducer,
 });
 
 export default rootReducer;
