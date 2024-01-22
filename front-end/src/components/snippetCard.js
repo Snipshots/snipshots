fc9611aa6cae49c97285290e397c76505c284bea
Kris Fragata <kristinnefragata@outlook.com>
2024-01-22 11:21:25 -0500

    adding comments to frontend
    kris <kristinnefragata@outlook.com
    julien <julien.kerekes@gmail.com

--- front-end/src/components/snippetCard.js
+++ front-end/src/components/snippetCard.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 const SnippetCard = ({ snippet, displaySnippet }) => {
   const { title, tags } = snippet;
 
+  // render the title and tags that are passed in as a snippet card
   return (
     <div className='snippetCard' onClick={displaySnippet}>
       <h2>{title}</h2>
